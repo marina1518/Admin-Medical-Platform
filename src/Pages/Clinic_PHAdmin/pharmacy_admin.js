2cb6712c98427d5ac5ed38058018a816f84b58df
marina-kamal-mk <74077914+marina-kamal-mk@users.noreply.github.com>
2022-03-07 00:49:04 +0200

    pharmacy admin

--- src/Pages/Clinic_PHAdmin/pharmacy_admin.js
+++ src/Pages/Clinic_PHAdmin/pharmacy_admin.js
@@ -5,6 +5,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {MdOutlineDoneOutline,MdOutlineDone,MdCancel} from 'react-icons/md';
 import Avatar from '@material-ui/core/Avatar';
 
+import { Chart as ChartJS } from 'chart.js/auto';
+
+import { Bar,Line, Pie, Chart } from "react-chartjs-2";
+
+
 const Ph_admin =()=>{
    
     const [state,setstate] = useState(null);
@@ -51,12 +56,77 @@ const Ph_admin =()=>{
     {id:"2",username:"jj" , order:"- k , - p " }
 ];
 const [done_list,setdone]=useState(approved);
+//var history=[];
+let [history,sethistory]=useState([]);
 const done=(e,id)=>{
     const newp = done_list.filter((item)=> item.id !== id );
     setdone(newp);
-   }
-   //console.log(done_list);
 
+    const newpp = done_list.filter((item)=> item.id === id );
+   
+   
+    for (let i = 0; i < newpp.length; i++) {
+     history.push(newpp[i])
+    }
+   sethistory(history);
+   }
+
+   
+
+
+  const labels= ['Jan','Feb','Mar','April','May','June','July','Aug','Sep', 'Oct','Nov','Dec'];
+
+   const  data={
+		labels: labels,
+		  datasets: [{
+			label: 'Price',
+			data: [65, 59, 80, 81, 56, 55, 40,90,25,45,89,20],
+			backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(0, 77, 64)',
+        'rgb(210,129,64)',
+        'rgb(183, 28, 28)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(73,95,117)',
+        'rgb(54, 162, 235)',
+        'rgb(106,62,115)',
+        'rgb(153, 102, 255)',
+        'rgb(144, 164, 174)',
+        'rgb(0, 0, 0)'
+
+
+			  
+			],
+			hoverOffset: 4
+		  }, 
+      {
+        label: 'Quantity',
+        data: [65, 59, 80, 81, 56, 55, 40,90,25,45,89,20],
+        backgroundColor: [
+         
+			  'rgba(255, 99, 132, 0.5)',
+        'rgba(0, 77, 64,0.5)',
+        'rgba(210,129,64, 0.5)',
+        'rgba(183, 28, 28,0.5)',
+        'rgba(255, 205, 86, 0.5)',
+        'rgba(75, 192, 192, 0.5)',
+        'rgba(73,95,117,0.5)',
+        'rgba(54, 162, 235, 0.5)',
+        'rgba(106,62,115,0.5)',
+        'rgba(153, 102, 255, 0.5)',
+        'rgba(144, 164, 174, 0.5)',
+        'rgba(0, 0, 0, 0.5)'
+          
+        ],
+        hoverOffset: 4
+        }
+    
+    ]
+	};
+
+
+  const [report,setreport] = useState(false);
     return(
 
         <div className="student-profile py-4">
@@ -187,6 +257,58 @@ const done=(e,id)=>{
 }
 
         </Tab>
+
+
+        <Tab eventKey="History" title="History">
+          {/* <Sonnet /> */}
+          {(history.length === 0) ?  "No History":
+          <Accordion defaultActiveKey="0" flush>
+          {
+                  history.map((item)=>
+                 
+        <Accordion.Item eventKey={item}>
+            <Accordion.Header> {item.username}</Accordion.Header>
+            <Accordion.Body>
+           {item.order}
+          
+            </Accordion.Body>
+        </Accordion.Item>
+       
+                  )
+              }
+
+
+        <br/>
+
+<Button variant="primary" onClick={(e)=>setreport(true)}>Get Report</Button>
+
+
+{
+  report ? 
+  <Stack gap={3} className="p-2"> 
+			<div className="bg-light border rounded-md border-gray-400 max-w-screen-md  p-4 chart" id="rep1">
+			<h1 className="text-center text-3xl p-2 text-blue-900 font-bold">Monthly Report</h1>
+			<Bar className="bg-white"
+			title="Monthly Report" 
+			
+			data={data}
+			
+			
+			></Bar>
+		
+			
+				</div>
+				</Stack> :""
+}
+
+               </Accordion>
+}
+
+
+        </Tab>
+
+
+        
        
       </Tabs>
 
