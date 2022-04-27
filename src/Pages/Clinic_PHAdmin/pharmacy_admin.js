b9631f224aa844a1cb0318ca3f9e81aafe4cfebd
marina-kamal-mk <74077914+marina-kamal-mk@users.noreply.github.com>
2022-04-27 21:34:47 +0200

    pharmacy for admin

--- src/Pages/Clinic_PHAdmin/pharmacy_admin.js
+++ src/Pages/Clinic_PHAdmin/pharmacy_admin.js
@@ -1,48 +1,45 @@
 import React , {useEffect, useState} from "react";
 import './profile.css';
-import { Alert ,Button,ButtonGroup,ListGroup, Stack , Tab, Tabs, Accordion ,Col,Row} from "react-bootstrap";
+import { Button,ButtonGroup, Stack , Tab, Tabs, Accordion ,Col,Row} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {MdOutlineDoneOutline,MdOutlineDone,MdCancel} from 'react-icons/md';
 import Avatar from '@material-ui/core/Avatar';
-import {useSelector,useDispatch} from 'react-redux'
+import {useSelector} from 'react-redux'
 import axios from "axios";
-
-//import { Chart as ChartJS } from 'chart.js/auto';
-
-import { Bar,Line, Pie, Chart } from "react-chartjs-2";
+import { Bar} from "react-chartjs-2";
 
 
 const Ph_admin =()=>{
     const token = JSON.parse(useSelector(state => state.auth));
     const [state,setstate] = useState(null);
     const [edit_photo,setEdit_photo]=useState(false);
-    const orders =[
-        {id:"1", username:"kk" , order:"- m , - g " },
-        {id:"2",username:"jj" , order:"- k , - p " }
-    ];
-    const [neworders,setneworders]=useState(orders);
+   
+   // const [neworders,setneworders]=useState(orders);
     var[phorder,setphorder]=useState([]);
     const[order2_details,setorder2_details]=useState([]);
     const[pending_orders,setpending_details]=useState([]);
     var order_list = JSON.parse(JSON.stringify(phorder));
     var order_details = {};
-    console.log(token.token);
+    
     const config = {headers: {
     
       'Authorization': `Bearer ${token.token}`}};
+
     const Get_orders_Api = async ()=>{
       try {
-             const res = await axios.get(`https://future-medical.herokuapp.com/admin/orders/new`,config)
+             const res = await axios.get(`https://future-medical.herokuapp.com/admin/orders/new`, config)
              const data = await res.data;
-             console.log(data.order_data);
+             console.log(data);
              data.forEach((x) => {
               order_details.form = x.order_data.form;
-              order_details.Date = x.order_data.Date;
+              let b=x.order_data.Date.split("T")
+              order_details.Date =b[0];
               order_details.address = x.order_data.address;
               order_details.phone = x.order_data.phone;
               order_details.price = x.price;
               order_details.username = x.user.username;
               order_details.email = x.user.email;
+              order_details.id = x._id;
               order_list.push(order_details); 
               console.log(order_details);   
               order_details={};   
@@ -51,9 +48,9 @@ const Ph_admin =()=>{
                });
               setorder2_details(order_list); 
               
-              Get_pending_Api();
-              Get_approved_Api();
-              Get_history_Api();
+              // Get_pending_Api();
+              // Get_approved_Api();
+              // Get_history_Api();
          } 
          catch (err) {
              console.error(err);
@@ -74,12 +71,14 @@ const Ph_admin =()=>{
              {return}
              data.forEach((x) => {
               order_details.form = x.order_data.form;
-              order_details.Date = x.order_data.Date;
+              let b=x.order_data.Date.split("T")
+              order_details.Date =b[0];
               order_details.address = x.order_data.address;
               order_details.phone = x.order_data.phone;
               order_details.price = x.price;
               order_details.username = x.user.username;
               order_details.email = x.user.email;
+              order_details.id = x._id;
               order_list2.push(order_details); 
               console.log(order_details);   
               order_details={};   
@@ -107,11 +106,13 @@ const Ph_admin =()=>{
              {return}
              data.forEach((x) => {
               order_details.form = x.order_data.form;
-              order_details.Date = x.order_data.Date;
+              let b=x.order_data.Date.split("T")
+              order_details.Date =b[0];
               order_details.address = x.order_data.address;
               order_details.phone = x.order_data.phone;
               order_details.price = x.price;
               order_details.username = x.user.username;
+              order_details.id = x._id;
               order_details.email = x.user.email;
               order_list3.push(order_details); 
               console.log(order_details);   
@@ -141,12 +142,14 @@ const Ph_admin =()=>{
              {return}
              data.forEach((x) => {
               order_details.form = x.order_data.form;
-              order_details.Date = x.order_data.Date;
+              let b=x.order_data.Date.split("T")
+              order_details.Date =b[0];
               order_details.address = x.order_data.address;
               order_details.phone = x.order_data.phone;
               order_details.price = x.price;
               order_details.username = x.user.username;
               order_details.email = x.user.email;
+              order_details.id = x._id;
               order_list3.push(order_details); 
               console.log(order_details);   
               order_details={};   
@@ -160,60 +163,8 @@ const Ph_admin =()=>{
          }
 
      }
-  // console.log(neworders);
-   const remove=(e,id)=>{
-    const newp = neworders.filter((item)=> item.id !== id );
-    setneworders(newp);
-   }
-
-   let [app_orders,setapp_orders]=useState([]);
- //  const [new_app_orders,setnew_app_orders]=useState(app_orders);
+ 
   
-   const pending=(e,id)=>{
-     
-       const newp = orders.filter((item)=> item.id === id );
-      
-       //const order = app_orders.push(newp);
-      //app_orders.push(newp);
-    //  console.log(newp);
-      //app_orders.push({id:"1",username:"kk",order:"hh"});
-      for (let i = 0; i < newp.length; i++) {
-       app_orders.push(newp[i])
-      }
-      //app_orders = [...app_orders, [... newp]]
-        setapp_orders(app_orders);
-        remove(e,id);
-     //  setnew_app_orders(app_orders);
-       
-    
-   };
-   console.log(app_orders);
-  // console.log(new_app_orders);
-
-
-//   const approved =[
-//     {id:"1", username:"kk" , order:"- m , - g " },
-//     {id:"2",username:"jj" , order:"- k , - p " }
-// ];
-//const [done_list,setdone]=useState(approved);
-//var history=[];
-//let [history,sethistory]=useState([]);
-// const done=(e,id)=>{
-//     const newp = done_list.filter((item)=> item.id !== id );
-//     setdone(newp);
-
-//     const newpp = done_list.filter((item)=> item.id === id );
-   
-   
-//     for (let i = 0; i < newpp.length; i++) {
-//      history.push(newpp[i])
-//     }
-//    sethistory(history);
-//    }
-
-   
-
-
   const labels= ['Jan','Feb','Mar','April','May','June','July','Aug','Sep', 'Oct','Nov','Dec'];
 
    const  data={
@@ -264,16 +215,64 @@ const Ph_admin =()=>{
     
     ]
 	};
-  // const order_api =() =>{
-  //   Get_orders_Api();
-  // }
+ 
   useEffect(()=>{
-    /*Get_Admin_info().then((res)=>{console.log(res); setinfo(res)})   */
     Get_orders_Api();
+    Get_approved_Api();
+    Get_history_Api();
+    Get_pending_Api();
     
     },[]) 
+
     const[price,setPrice]=useState("");
+ 
   const [report,setreport] = useState(false);
+const [approve_data, setapprove_data] = useState("");
+const [done_id, setdone_id] = useState("");
+
+  const Approve_api = async ()=>{
+    try {
+           const res = await axios.patch(`https://future-medical.herokuapp.com/admin/order/approve`, approve_data, config )
+           const data = await res.data;
+           console.log(data);
+            
+           
+            
+       } 
+       catch (err) {
+           console.error(err);
+       }
+
+   }
+
+   const Done_Api = async ()=>{
+    try {
+           const res = await axios.patch(`https://future-medical.herokuapp.com/admin/order/done`, done_id, config )
+           const data = await res.data;
+           console.log(data);
+            
+           
+            
+       } 
+       catch (err) {
+           console.error(err);
+       }
+
+   }
+  const approve=(e,id)=>{
+ 
+    const o={id:id, price:price};
+   
+    setapprove_data(o);
+    console.log(approve_data)
+   Approve_api();
+  }
+
+  const Done=(e,id)=>{
+    const o={id:id};
+    setdone_id(o);
+    Done_Api();
+  }
     return(
 
         <div className="student-profile py-4">
@@ -283,37 +282,15 @@ const Ph_admin =()=>{
         <div className="card shadow-sm">
              
           <div className="card-header bg-transparent text-center">
-
-            {/* <img className="profile_img" src={user_data.pic} alt="student dp"/> */}
-            {/* <Avatar className="profile_img"  alt="Remy Sharp" src="/broken-image.jpg" >
-                B
-            </Avatar> */}
-           {/* <div>
-               
-           <Avatar className="profile_img" src="/broken-image.jpg" />
-           <Button onClick={(e)=>setEdit_photo(true)}>
-           <BackupIcon ></BackupIcon> upload
-           </Button>
-           {edit_photo ? <input type="file"></input>:""}
-           
-           </div> */}
            <Avatar className="profile_img" src="/broken-image.jpg" onClick={(e)=>setEdit_photo(true)} />
            {edit_photo ? <input type="file"></input>:""}
-         
-            
-          
             <h3>Pharmacy </h3>
           </div>
           <div className="card-body">
             <p className="mb-0"><strong className="pr-1">Email: </strong>gh</p>
-            
-            
+            </div>
           </div>
-          </div>
-          
       </div>
-     
-
       <br/>
       <div className="col-lg-8">
         <div className="card shadow-sm">
@@ -325,13 +302,12 @@ const Ph_admin =()=>{
             <br/>
               
         <Tabs
-        defaultActiveKey="home"
+        defaultActiveKey="Orders"
         transition={false}
         id="noanim-tab-example"
         className="mb-3"
       >
         <Tab eventKey="Orders" title="Orders" >
-          {/* <Sonnet /> */}
           {(order2_details.length === 0) ?  "No Notifications":
           <Accordion defaultActiveKey="0" flush>
           {
@@ -347,15 +323,16 @@ const Ph_admin =()=>{
            <img src={item.form} width="300px" height="300px"/>
            <h3>Address : {item.address}</h3>
            <h3>Phone : {item.phone}</h3>
+           <h3>Email : {item.email}</h3>
            <br/>
            <Row>
            <Col>
-            <input type="text" placeholder="Price" onChange={(e)=>{setPrice(e.event.target)}}/>
+            <input type="text" placeholder="Price" onChange={(e)=>{setPrice(parseFloat(e.target.value))}}/>
            </Col>
           <Col>
             <ButtonGroup>
-              <Button variant="outline-success" className="col-md-12 text-right" onClick={(e)=>pending(e,item.id)}><MdOutlineDone/></Button>
-              <Button variant="outline-danger" className="col-md-12 text-right" onClick={(e)=>remove(e,item.id)}><MdCancel/></Button>
+              <Button variant="outline-success" className="col-md-12 text-right" onClick={(e)=>approve(e,item.id)} ><MdOutlineDone/></Button>
+              <Button variant="outline-danger" className="col-md-12 text-right" ><MdCancel/></Button>
             </ButtonGroup>
           </Col>
           </Row>
@@ -386,6 +363,8 @@ const Ph_admin =()=>{
                <img src={item.form} width="300px" height="300px"/>
                 <h3>Address : {item.address}</h3>
                 <h3>Phone : {item.phone}</h3>
+                <h3>Email : {item.email}</h3>
+                <h3>Price : {item.price} LE</h3>
                </Accordion.Body>
              
            </Accordion.Item>
@@ -415,9 +394,11 @@ const Ph_admin =()=>{
                <img src={item.form} width="300px" height="300px"/>
                 <h3>Address : {item.address}</h3>
                 <h3>Phone : {item.phone}</h3>
-                <h3>Price : {item.price}</h3>
+                <h3>Email : {item.email}</h3>
+                <h3>Price : {item.price} LE</h3>
+
                <br/>
-             <Button variant="outline-success" className="col-md-12 text-right" >Done <MdOutlineDoneOutline/></Button>
+             <Button variant="outline-success" className="col-md-12 text-right" onClick={(e)=>Done(e,item.id)} >Done <MdOutlineDoneOutline/></Button>
                </Accordion.Body>
            </Accordion.Item>
           
@@ -445,7 +426,8 @@ const Ph_admin =()=>{
                <img src={item.form} width="300px" height="300px"/>
                 <h3>Address : {item.address}</h3>
                 <h3>Phone : {item.phone}</h3>
-                <h3>Price : {item.price}</h3>
+                <h3>Email : {item.email}</h3>
+                <h3>Price : {item.price} LE</h3>
                </Accordion.Body>
         </Accordion.Item>
        
