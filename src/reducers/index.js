4aacb8c1c1c144d3eec00008e2aff864813ba2b8
Madonna88 <76403994+Madonna88@users.noreply.github.com>
2022-05-09 19:33:21 +0200

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,12 @@
 import sidebarstate_reducer from "./sidebarcomp";
 
 import auth_reducer from "./islogged";
-
+import profile_reducer from "./Profilesidebar";
 import {combineReducers} from 'redux'
 
 const allreducers = combineReducers ({
    sidebarcomp:sidebarstate_reducer,
+   profile_reducer:profile_reducer,
    auth:auth_reducer
 })
 
