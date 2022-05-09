3c1823eccbd53317c1d83b88c462cd4b91382da7
marina-kamal-mk <74077914+marina-kamal-mk@users.noreply.github.com>
2022-05-09 22:21:29 +0200

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import sidebarstate_reducer from "./sidebarcomp";
 import meeting_reducer from "./meeting";
 import auth_reducer from "./islogged";
-<<<<<<< HEAD
+
 import patient_reducer from './patient'
 import {combineReducers} from 'redux';
 
@@ -10,15 +10,7 @@ const allreducers = combineReducers ({
    auth:auth_reducer,
   meeting_reducer:meeting_reducer,
   patient_reducer:patient_reducer
-=======
-import profile_reducer from "./Profilesidebar";
-import {combineReducers} from 'redux'
 
-const allreducers = combineReducers ({
-   sidebarcomp:sidebarstate_reducer,
-   profile_reducer:profile_reducer,
-   auth:auth_reducer
->>>>>>> b5b89da1d4c4750cf6fe5cf0635e8ab196b18f24
 })
 
 export default allreducers ;
