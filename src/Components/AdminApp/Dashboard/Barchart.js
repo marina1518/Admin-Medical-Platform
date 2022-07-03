2e4f5a85cad49bacd7ca446a82f57c018254474f
Fady Zakaria <fady.zakaria93@gmail.com>
2022-07-03 14:39:41 +0200

    Admin_App_UPDATED

--- src/Components/AdminApp/Dashboard/Barchart.js
+++ src/Components/AdminApp/Dashboard/Barchart.js
@@ -1,12 +1,20 @@
-import React, { useEffect , useState} from 'react';
-import './chart.css'
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import React, { useEffect, useState } from "react";
+import "./chart.css";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
-
-import { Card, Stack } from 'react-bootstrap';
-import axios from 'axios'
-import { useSelector } from 'react-redux';
-
+import { Card, Stack } from "react-bootstrap";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 /*const data = [
   {
@@ -53,7 +61,8 @@ import { useSelector } from 'react-redux';
   },
 ];*/
 
-    {/*return (
+{
+  /*return (
         <Card className="chart">
         <div>
         <h3 className="chartTitle">AGE USERS</h3>
@@ -82,70 +91,75 @@ import { useSelector } from 'react-redux';
       </Card>
     );
   
-}*/}
-
-function Barchart()  {
-    let one_age = {} ;
-let age_list = [];
-const [data,setdata] = useState([]);
-     const token = JSON.parse(useSelector(state => state.auth)); //state of token 
-     
-const Ages_barchart_Api = async ()=>{
- try {
-        const res = await axios.get('https://future-medical.herokuapp.com/admin/dashboard/ages',{
-          headers: {
-          'Authorization': `Bearer ${token.token}`
-          }
-        })
-        const data = await res.data;
-        console.log(data);
-        for(let x in data){
-            one_age.name = x ;
-            one_age.males = data[x][0];
-            one_age.females = data[x][1];
-            age_list.push(one_age);
-            one_age={}
-                /*console.log("dic[x]=",x);
-                console.log("male count =",data[x][0]);
-                console.log("female count =",data[x][1]);*/
-            }
-        setdata(age_list); 
-        console.log("age_list",age_list)
-    } 
-    catch (err) {
-        console.error(err);
-    }
+}*/
 }
 
-useEffect(()=>{
-    Ages_barchart_Api()
-},[])
+function Barchart() {
+  let one_age = {};
+  let age_list = [];
+  const [data, setdata] = useState([]);
+  const token = JSON.parse(useSelector((state) => state.auth)); //state of token
+
+  const Ages_barchart_Api = async () => {
+    try {
+      const res = await axios.get(
+        "https://future-medical.herokuapp.com/admin/dashboard/ages",
+        {
+          headers: {
+            Authorization: `Bearer ${token.token}`,
+          },
+        }
+      );
+      const data = await res.data;
+      console.log(data);
+      for (let x in data) {
+        one_age.name = x;
+        one_age.males = data[x][0];
+        one_age.females = data[x][1];
+        age_list.push(one_age);
+        one_age = {};
+        /*console.log("dic[x]=",x);
+                console.log("male count =",data[x][0]);
+                console.log("female count =",data[x][1]);*/
+      }
+      setdata(age_list);
+      console.log("age_list", age_list);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    Ages_barchart_Api();
+  }, []);
   return (
-      <Card className="chart">
-        <div>
+    <Card className="chart">
+      <div>
         <h3 className="chartTitle">Users Analytics with ages</h3>
-    <BarChart style={{'margin' : '0 auto'}}
-      width={600}
-      height={300}
-      data={data}
-      
-      /*margin={{
+        <BarChart
+          className="bar-chart-edit"
+          style={{ margin: "0 auto" }}
+          width={600}
+          height={300}
+          data={data}
+
+          /*margin={{
         top: 20,
         right: 30,
         left: 20,
         bottom: 5
       }}*/
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="males" stackId="a" fill="#8884d8" />
-      <Bar dataKey="females" stackId="a" fill="#82ca9d" />
-    </BarChart>
-       </div>
-      </Card>
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="males" stackId="a" fill="#8884d8" />
+          <Bar dataKey="females" stackId="a" fill="#82ca9d" />
+        </BarChart>
+      </div>
+    </Card>
   );
 }
-export default Barchart
+export default Barchart;
--- src/Components/AdminApp/Dashboard/Customchart.js
+++ src/Components/AdminApp/Dashboard/Customchart.js
@@ -1,279 +1,291 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 import Chart from "react-google-charts";
-import { Container , Row , Col ,Card , Alert ,Form , Button} from 'react-bootstrap'
-import axios, * as others from 'axios';
-import { useSelector } from 'react-redux';
 import {
-  ResponsiveContainer,
-} from "recharts";
-import './chart.css'
+  Container,
+  Row,
+  Col,
+  Card,
+  Alert,
+  Form,
+  Button,
+} from "react-bootstrap";
+import axios, * as others from "axios";
+import { useSelector } from "react-redux";
+import { ResponsiveContainer } from "recharts";
+import "./chart.css";
 
 function generateRandomColor(Colors_list) {
-    console.log("color_list",Colors_list);
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  var flag = true ;
-  while (flag){
-    flag = false ;
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  for (var i = 0 ; i < Colors_list.length ; i++)
-  {
-    if (color === Colors_list[i])
-    {
+  console.log("color_list", Colors_list);
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  var flag = true;
+  while (flag) {
+    flag = false;
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    for (var i = 0; i < Colors_list.length; i++) {
+      if (color === Colors_list[i]) {
         flag = true;
-        break ;
+        break;
+      }
     }
   }
-}
   return color;
 }
 
-
-
 export default function Customchart(props) {
+  const token = JSON.parse(useSelector((state) => state.auth)); //state of token
 
- const token = JSON.parse(useSelector(state => state.auth)); //state of token 
+  const [data1, setdata1] = useState([]);
+  const [show, setshow] = useState(false);
+  const [no_profit_flag, set_no_profit] = useState(false);
+  let hospitals_list = [];
+  let clinics_list = [];
+  let pharmacies_list = [];
+  let color_list = [];
+  let col = "";
+  let temp_list = [];
+  let start = new Date();
+  let end = new Date();
 
- const [data1,setdata1]=useState([]);
- const [show,setshow]=useState(false);
- const [no_profit_flag,set_no_profit]=useState(false);
- let hospitals_list = [];
- let clinics_list = [];
- let pharmacies_list = [];
- let color_list = [];
- let col = "";
- let temp_list = [];
-   let start = new Date;
-   let end = new Date ;
-
- const Profit_hospitals_Api = async ()=>{
+  const Profit_hospitals_Api = async () => {
     start = FormValues.startdate;
     end = FormValues.enddate;
-    console.log("start date" , start);
-    console.log("end date" , end);
+    console.log("start date", start);
+    console.log("end date", end);
 
- try {
-        const res = await axios.get(`https://future-medical.herokuapp.com/admin/dashboard/profit/${start}/${end}`,{
+    try {
+      const res = await axios.get(
+        `https://future-medical.herokuapp.com/admin/dashboard/profit/${start}/${end}`,
+        {
           headers: {
-          'Authorization': `Bearer ${token.token}`
-          }
-        })
-        const data = await res.data;
-        console.log(data);
-        
-        //console.log(hospitals)
-        clinics_list.push([
-             'Element',
-             'Profit',
-      { role: 'style' },
-      {
-        sourceColumn: 0,
-        role: 'annotation',
-        type: 'string',
-        calc: 'stringify',
-      },
-    ]);
+            Authorization: `Bearer ${token.token}`,
+          },
+        }
+      );
+      const data = await res.data;
+      console.log(data);
 
-        pharmacies_list.push([
-             'Element',
-             'Profit',
-      { role: 'style' },
-      {
-        sourceColumn: 0,
-        role: 'annotation',
-        type: 'string',
-        calc: 'stringify',
-      },
-    ]);
+      //console.log(hospitals)
+      clinics_list.push([
+        "Element",
+        "Profit",
+        { role: "style" },
+        {
+          sourceColumn: 0,
+          role: "annotation",
+          type: "string",
+          calc: "stringify",
+        },
+      ]);
 
-        hospitals_list.push([
-             'Element',
-             'Profit',
-      { role: 'style' },
-      {
-        sourceColumn: 0,
-        role: 'annotation',
-        type: 'string',
-        calc: 'stringify',
-      },
-    ]);
-        for(let x in data.hospitals){            
-            col = generateRandomColor(color_list);
-            color_list.push(col)
-            temp_list=[x,data.hospitals[x],col,null];         
-            console.log("temp list", temp_list )
-            hospitals_list.push(temp_list);
-            temp_list=[]
-            col=""
-            
-        }
-        color_list=[];
-         for(let x in data.clinics){
-            col = generateRandomColor(color_list);
-            color_list.push(col)
-            temp_list=[x,data.clinics[x],col,null];
-         
-            console.log("temp list", temp_list )
-            clinics_list.push(temp_list);
-            temp_list=[]
-            col =""
-        }
-        color_list=[];
-          for(let x in data.pharmacies){
-             col = generateRandomColor(color_list);
-            color_list.push(col)
-            temp_list=[x,data.pharmacies[x],col,null];
-         
-            console.log("temp list", temp_list )
-            pharmacies_list.push(temp_list);
-            temp_list=[]
-            col=""
-        }
-        color_list=[];
-        //['Women', 24, generateRandomColor(), null]
-        console.log("hospitals_list",hospitals_list)
+      pharmacies_list.push([
+        "Element",
+        "Profit",
+        { role: "style" },
+        {
+          sourceColumn: 0,
+          role: "annotation",
+          type: "string",
+          calc: "stringify",
+        },
+      ]);
 
-        if (props.parent === "hospitals" )
+      hospitals_list.push([
+        "Element",
+        "Profit",
+        { role: "style" },
         {
-          setdata1(hospitals_list)
-          if(hospitals_list.length>1){
-            setshow(true);
-          }
-          else {
-            set_no_profit(true)
-          }
-        }
-        else if (props.parent === "clinics")
-        {
-             setdata1(clinics_list)
-            if(clinics_list.length>1){
-            setshow(true);
-          }
-           else {
-            set_no_profit(true)
-          }
-        }
-        else if (props.parent === "pharmacies")
-        {
-             setdata1(pharmacies_list)
-                if(pharmacies_list.length>1){
-            setshow(true);
-          }
-           else {
-            set_no_profit(true)
-          }
-        }
-        
-    } 
-    catch (err) {
-        console.error(err);
-    }
-}
-const [FormValues,setFormvalues]= useState({});
-const [issubmit,setissubmit]= useState(false);
-const [Formerrors,setFormerrors]= useState({});
-const handlechange = (e)=>{
-         console.log(e.target)
-         //const { name , value} = e.target;
-         const name = e.target.name ;
-         const value = e.target.value ;
-         console.log(value);
-         setFormvalues({...FormValues, [name] : value});
-         
-         if (issubmit)
-         {
-            setFormerrors(validate({...FormValues, [name] : value}))
-         }
-         set_no_profit(false); //in change the date 
-         setshow(false); //in change the date  
-        // console.log(FormValues);
-    }
-    function validate (values)
-    {
-        const errors = {};
-        
+          sourceColumn: 0,
+          role: "annotation",
+          type: "string",
+          calc: "stringify",
+        },
+      ]);
+      for (let x in data.hospitals) {
+        col = generateRandomColor(color_list);
+        color_list.push(col);
+        temp_list = [x, data.hospitals[x], col, null];
+        console.log("temp list", temp_list);
+        hospitals_list.push(temp_list);
+        temp_list = [];
+        col = "";
+      }
+      color_list = [];
+      for (let x in data.clinics) {
+        col = generateRandomColor(color_list);
+        color_list.push(col);
+        temp_list = [x, data.clinics[x], col, null];
 
-        if (!values.startdate)
-            {
-                errors.startdate="start date is required!";  
-            }
-        
-        if (!values.enddate)
-            {
-                errors.enddate="end date is required!";  
-            }
-         
-        return errors ;
-    }    
-const submithandle =(e)=>{
-        e.preventDefault();
-        setFormerrors(validate(FormValues))
-        setissubmit(true);
-        //setshow(true)
-        if(Object.keys(validate(FormValues)).length === 0)
-        {
-            //empty
-            setissubmit(true);
-            //POST
-             Profit_hospitals_Api()  
+        console.log("temp list", temp_list);
+        clinics_list.push(temp_list);
+        temp_list = [];
+        col = "";
+      }
+      color_list = [];
+      for (let x in data.pharmacies) {
+        col = generateRandomColor(color_list);
+        color_list.push(col);
+        temp_list = [x, data.pharmacies[x], col, null];
+
+        console.log("temp list", temp_list);
+        pharmacies_list.push(temp_list);
+        temp_list = [];
+        col = "";
+      }
+      color_list = [];
+      //['Women', 24, generateRandomColor(), null]
+      console.log("hospitals_list", hospitals_list);
+
+      if (props.parent === "hospitals") {
+        setdata1(hospitals_list);
+        if (hospitals_list.length > 1) {
+          setshow(true);
+        } else {
+          set_no_profit(true);
+        }
+      } else if (props.parent === "clinics") {
+        setdata1(clinics_list);
+        if (clinics_list.length > 1) {
+          setshow(true);
+        } else {
+          set_no_profit(true);
+        }
+      } else if (props.parent === "pharmacies") {
+        setdata1(pharmacies_list);
+        if (pharmacies_list.length > 1) {
+          setshow(true);
+        } else {
+          set_no_profit(true);
         }
       }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  const [FormValues, setFormvalues] = useState({});
+  const [issubmit, setissubmit] = useState(false);
+  const [Formerrors, setFormerrors] = useState({});
+  const handlechange = (e) => {
+    console.log(e.target);
+    //const { name , value} = e.target;
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(value);
+    setFormvalues({ ...FormValues, [name]: value });
 
+    if (issubmit) {
+      setFormerrors(validate({ ...FormValues, [name]: value }));
+    }
+    set_no_profit(false); //in change the date
+    setshow(false); //in change the date
+    // console.log(FormValues);
+  };
+  function validate(values) {
+    const errors = {};
 
-/*useEffect(()=>{
+    if (!values.startdate) {
+      errors.startdate = "start date is required!";
+    }
+
+    if (!values.enddate) {
+      errors.enddate = "end date is required!";
+    }
+
+    return errors;
+  }
+  const submithandle = (e) => {
+    e.preventDefault();
+    setFormerrors(validate(FormValues));
+    setissubmit(true);
+    //setshow(true)
+    if (Object.keys(validate(FormValues)).length === 0) {
+      //empty
+      setissubmit(true);
+      //POST
+      Profit_hospitals_Api();
+    }
+  };
+
+  /*useEffect(()=>{
     Profit_hospitals_Api();
 },[])*/
 
-    return(
-       <Card className="chart">
-        <div>
+  return (
+    <Card className="chart">
+      <div>
         <h3 className="chartTitle">{props.title}</h3>
-        
-          <Form onSubmit={submithandle} className="rounded p-4" style={{width:'50%' , margin:'0 25%'}}>
-  <Row className="mb-3">
-    <Form.Group as={Col} controlId="formGridCity">
-      <Form.Label>Start date</Form.Label>
-      <Form.Control type='date' name='startdate'  value={FormValues.startdate} onChange={handlechange}/>
-      <p style={{padding:'0',color:'red' , marginTop:'6px'}} >{Formerrors.startdate}</p> 
-    </Form.Group>
 
-    <Form.Group as={Col} controlId="formGridZip">
-      <Form.Label>End date </Form.Label>
-      <Form.Control type='date' name='enddate' value={FormValues.enddate} onChange={handlechange}/>
-      <p style={{padding:'0',color:'red', marginTop:'6px'}} >{Formerrors.enddate}</p>     
-    </Form.Group>
-  </Row>
+        <Form
+          onSubmit={submithandle}
+          className="rounded p-4"
+          style={{ width: "50%", margin: "0 25%" }}
+        >
+          <Row className="mb-3">
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Label>Start date</Form.Label>
+              <Form.Control
+                type="date"
+                name="startdate"
+                value={FormValues.startdate}
+                onChange={handlechange}
+              />
+              <p style={{ padding: "0", color: "red", marginTop: "6px" }}>
+                {Formerrors.startdate}
+              </p>
+            </Form.Group>
 
-  <Button style={{marginLeft:'27%' , marginTop:'15px' , width:'40%'}} variant="primary" type="submit">
-   Show
-  </Button>
-</Form>
-     
-     { no_profit_flag && <p style={{fontSize:'25px'}}>No profit in this period</p>}
-     { show && <Chart style={{marginBottom:'100px'}}
-  width={'500px'}
-  height={'300px'}
-  chartType="ColumnChart"
-  loader={<div>Loading Chart</div>}
-  data={data1}
-  options={{
-    title: `Profit of ${props.parent}`,
-    width: 900,
-    height: 400,
-    scales: {
-            y: {
-                beginAtZero: true
-            } },
-    bar: { groupWidth: '60%' },
-    legend: { position: 'none' },
-  }}
-/>}
+            <Form.Group as={Col} controlId="formGridZip">
+              <Form.Label>End date </Form.Label>
+              <Form.Control
+                type="date"
+                name="enddate"
+                value={FormValues.enddate}
+                onChange={handlechange}
+              />
+              <p style={{ padding: "0", color: "red", marginTop: "6px" }}>
+                {Formerrors.enddate}
+              </p>
+            </Form.Group>
+          </Row>
 
-  </div>
-  
-      </Card>
-    )
+          <Button
+            style={{ marginLeft: "27%", marginTop: "15px", width: "40%" }}
+            variant="primary"
+            type="submit"
+          >
+            Show
+          </Button>
+        </Form>
+
+        {no_profit_flag && (
+          <p className="no-profit">No profit in this period</p>
+        )}
+        {show && (
+          <Chart
+            style={{ marginBottom: "100px" }}
+            width={"100%"}
+            height={"300px"}
+            chartType="ColumnChart"
+            loader={<div>Loading Chart</div>}
+            data={data1}
+            options={{
+              title: `Profit of ${props.parent}`,
+              width: 900,
+              height: 400,
+              scales: {
+                y: {
+                  beginAtZero: true,
+                },
+              },
+              bar: { groupWidth: "60%" },
+              legend: { position: "none" },
+            }}
+          />
+        )}
+      </div>
+    </Card>
+  );
 }
